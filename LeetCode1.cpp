d77ac0576188207e5722f6163ae2ec1663817e96
followDm <62467194+followDm@users.noreply.github.com>
2021-09-19 18:06:45 +0800

    Add files via upload

---
+++ LeetCode1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    vector<int> nums;
+    int n = 0, t = 0, target;
+    cout << "number of nums: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        cin >> t;
+        nums.push_back(t);
+    }
+    
+    cout << "target: ";
+    cin >> target;
+    Solution sol;
+    nums = sol.twoSum(nums, target);
+    n = nums.size();
+    cout << "[";
+    for (int i = 0; i < n - 1; i++){
+        cout << nums.at(i) << ",";
+    }
+    cout << nums.at(n - 1) << "]";
+    return 0;
+}
+
+// number of nums: 4
+// 2 7 11 15
+// target: 9
+// [0,1]
